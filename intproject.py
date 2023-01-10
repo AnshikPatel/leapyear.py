d3f28cb4a88a6770e8f454cc754b07cf53cd53ce
Anshik <119244261+AnshikPatel@users.noreply.github.com>
2023-01-10 14:27:11 +0530

    Add files via upload

---
+++ intproject.py
@@ -0,0 +1,19 @@
+# year=int(input("Enter the start year: "))
+# end=int(input("Enter the end year: "))
+# list =[]
+# for i in range(year,end):
+#     if i%400==0:
+#         list.append(i)
+#     elif i%4==0:
+#         if i%100!=0:
+#             list.append(i)
+    
+# print("Leap year are: \n",list)
+# list1 =[]
+# for i in range(year,end):
+#     if i%4!=0:
+#         if i%100!=0:
+#             list1.append(i)
+
+# print("Non-leap year are: \n", list1)
+def tantra
